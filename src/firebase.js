36f845983208bd6508c7eab5cd8b6773ec2eba11
Jordan Albers <albersjordan13@gmail.com>
2020-09-19 03:29:52 -0700

    completed basic cart features before adding stripe

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,6 @@
+import firebase from 'firebase';
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyCC8vD2yiyHwAysKNaFmyuI641J2yIXv5I",
     authDomain: "challenge-7a25a.firebaseapp.com",
@@ -8,3 +11,10 @@ const firebaseConfig = {
     appId: "1:663465343295:web:f09ad365b191d00141fb8a",
     measurementId: "G-MST4MTK3BE"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  
+  export { db, auth };
