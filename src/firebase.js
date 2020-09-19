ea9c56027d7d7aa0dffd453ad2de6011684c2302
Jordan Albers <albersjordan13@gmail.com>
2020-09-19 01:45:24 -0700

    finished home page and header styling

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCC8vD2yiyHwAysKNaFmyuI641J2yIXv5I",
+    authDomain: "challenge-7a25a.firebaseapp.com",
+    databaseURL: "https://challenge-7a25a.firebaseio.com",
+    projectId: "challenge-7a25a",
+    storageBucket: "challenge-7a25a.appspot.com",
+    messagingSenderId: "663465343295",
+    appId: "1:663465343295:web:f09ad365b191d00141fb8a",
+    measurementId: "G-MST4MTK3BE"
+  };
